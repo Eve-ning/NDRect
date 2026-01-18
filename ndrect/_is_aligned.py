7c5c97faaf0175d66a3696fd25334388d175a8bb
Eve-ning <johnchangqi@hotmail.com>
2026-01-18 16:44:37 +0800

    Make `repeat` always construct on a new alignment if already aligned

--- ndrect/_is_aligned.py
+++ ndrect/_is_aligned.py
@@ -118,7 +118,9 @@ class IsAligned(ABC):
             A new :class:`NDRectComplex` representing the repeated sequence.
 
         """
-        if isinstance(self, self._singular_type):
+        if isinstance(self, self._singular_type) or (
+            isinstance(self, self._complex_type) and self.aligned
+        ):
             return self._complex_type(rects=[self] * n)
         elif isinstance(self, self._complex_type):
             return self._complex_type(rects=self.rects * n)
