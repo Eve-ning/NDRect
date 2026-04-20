76c0c8da693aefcfcf9b5d85a40241e4c3ebc801
Eve-ning <johnchangqi@hotmail.com>
2026-04-20 21:37:26 +0800

    Fix incorrect pathing to complex and expose Unaligned Error exception

--- ndrect/__init__.py
+++ ndrect/__init__.py
@@ -1,5 +1,6 @@
 """ndrect - A Python library for N-dimensional rectangles (hyperrectangles)."""
 
-from ndrect.ndrect import NDRect, NDRectComplex
+from ndrect.ndrect import NDRect
+from ndrect.ndrect_complex import NDRectComplex, UnalignedError
 
 __all__ = ["NDRect", "NDRectComplex"]
